628405a6ff7738f360f113c4854d9698b08e1d84
abhav1947 <66078840+abhav1947@users.noreply.github.com>
2026-04-26 21:06:10 +0530

    Create weighted_constraint_2.sv

---
+++ weighted_constraint_2.sv
@@ -0,0 +1,23 @@
+/*A cyclic variable cannot really obey weighted probability in the normal sense, because once a value is used, it should not repeat until the cycle is exhausted.*/
+class pkt;
+  
+  rand bit [3:0] addr;
+  
+  extern constraint addr_trim;
+  
+endclass
+
+constraint pkt::addr_trim{addr dist {4:/10,[5:10]:/40};};
+
+module tb;
+  
+  pkt p1;
+  
+  initial begin
+    p1 = new();
+    repeat(20) begin
+      assert(p1.randomize()) else $display("Randomization Failed");
+      $display("addr = %0d", p1.addr);
+    end
+  end
+endmodule
